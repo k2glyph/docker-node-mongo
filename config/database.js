94271daab0a7dca93b0f77abb8ffc4d840245b3e
Ian Sinnott <ian@iansinnott.com>
2015-07-20 13:44:02 -0700

    Fix typo in db

--- config/database.js
+++ config/database.js
@@ -3,7 +3,7 @@ var mongoose = require('mongoose');
 var MONGO_URL = 'mongodb://localhost/';
 var DB_NAME = 'example-app-db';
 
-if (process.env.MONGO_PORT) MONGO_URL = 'mongodb://mongo';
+if (process.env.MONGO_PORT) MONGO_URL = 'mongodb://mongo/';
 
 mongoose.connect(MONGO_URL + DB_NAME);
 
