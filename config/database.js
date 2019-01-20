ab7b14fc66f9960c4eaac98e6095072d8446230b
Dinesh katwal <medineshkatwal@gmail.com>
2019-01-20 14:55:10 +0545

    Update database.js

--- config/database.js
+++ config/database.js
@@ -1,8 +1,8 @@
 var mongoose = require('mongoose');
-var MONGO_URL = 'mongodb://localhost/';
+var MONGO_URL = 'mongodb://mongo-0.mongo,mongo-1.mongo/';
 var DB_NAME = 'example-app-db';
 
-if (process.env.MONGO_PORT) MONGO_URL = 'mongodb://mongo/';
+if (process.env.MONGO_PORT) MONGO_URL = 'mongodb://mongo-0.mongo,mongo-1.mongo/';
 
 mongoose.connect(MONGO_URL + DB_NAME);
 
